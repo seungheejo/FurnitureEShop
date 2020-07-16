303f7189135b3822bf8a26de814cb199272c63ce
SeungheeJo <36188903+seungheejo@users.noreply.github.com>
2020-07-16 22:08:48 +0900

    Update SCustomerMapper.java
    주석 언어 변경(일본어->한국어)

--- src/main/java/org/jsp/board/service/SCustomerMapper.java
+++ src/main/java/org/jsp/board/service/SCustomerMapper.java
@@ -6,19 +6,19 @@ import org.jsp.board.vo.SCustomer;
 
 public interface SCustomerMapper {
 
-	// 会員加入 insert
+	// 회원가입 insert
 	public int joinCustomer(SCustomer sc);
 
-	// 会員情報の修正 update
+	// 회원정보 수정 update
 	public int updateCustomer(SCustomer sc);
 
-	// 脱退 delete
+	// 탈퇴 delete
 	public int deleteCustomer(HashMap<String, String> map);
 
-	// ログイン select
+	// 로그인 select
 	public SCustomer login(SCustomer sc);
 
-	// idを確認 select
+	// id확인 select
 	public String checkId(String custid);
 
 }
